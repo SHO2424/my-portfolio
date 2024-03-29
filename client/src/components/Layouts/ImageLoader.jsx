724c127ea5c0249e6413d359dcc63ba8af22ed2c
Shoei Akiwa <shoei@ShoeisMacBook-Air-3.local>
2024-03-29 13:30:58 +0900

    imageLoader

--- client/src/components/Layouts/ImageLoader.jsx
+++ client/src/components/Layouts/ImageLoader.jsx
@@ -6,26 +6,25 @@ function ImageLoader({ src, sx }) {
     const [error, setError] = useState(false);
     const [retryCount, setRetryCount] = useState(0);
     const maxRetry = 100; // リトライの最大回数
-
     useEffect(() => {
-        // 画像読み込みのための関数
-        const loadImage = () => {
-            setLoaded(false);
-            setError(false);
+      // 画像読み込みのための関数
+      const loadImage = () => {
+          setLoaded(false);
+          setError(false);
 
-            const image = new Image();
-            image.src = src;
-            image.onload = () => setLoaded(true);
-            image.onerror = () => {
-                if (retryCount < maxRetry) {
-                    setRetryCount(retryCount + 1);
-                } else {
-                    setError(true);
-                }
-            };
-        };
-        loadImage();
-    }, [src, retryCount]);
+          const image = new Image();
+          image.src = src;
+          image.onload = () => setLoaded(true);
+          image.onerror = () => {
+              if (retryCount < maxRetry) {
+                  setRetryCount(retryCount + 1);
+              } else {
+                  setError(true);
+              }
+          };
+      };
+      loadImage();
+  }, [src, retryCount]);
 
     if (error) {
         return <div>Error loading image.</div>;
