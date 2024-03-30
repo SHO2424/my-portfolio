eaa6a13c4eaedb19283336ddc791de5187497fb1
Shoei Akiwa <shoei@ShoeisMacBook-Air-3.local>
2024-03-31 00:36:58 +0900

    axios

--- client/src/lib/laravelAxios.js
+++ client/src/lib/laravelAxios.js
@@ -10,9 +10,13 @@ const laravelAxios = Axios.create({
 });
 
 laravelAxios.interceptors.request.use(config => {
-    const token = document.cookie.split('; ').find(row => row.startsWith('XSRF-TOKEN='));
+    const token = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('XSRF-TOKEN='));
     if (token) {
-        config.headers['X-XSRF-TOKEN'] = decodeURIComponent(token.split('=')[1]);
+        config.headers['X-XSRF-TOKEN'] = decodeURIComponent(
+            token.split('=')[1],
+        );
     }
     return config;
 });
