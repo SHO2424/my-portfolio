eac4052a40f407ab5e94e0ab86244d6a9ce81df9
Shoei Akiwa <shoei@ShoeisMacBook-Air-3.local>
2024-03-31 19:37:07 +0900

    csrf

--- client/src/lib/laravelAxios.js
+++ client/src/lib/laravelAxios.js
@@ -1,10 +1,17 @@
 import Axios from 'axios';
 
+function getCsrfToken() {
+    return document.cookie
+        .split('; ')
+        .find(row => row.startsWith('XSRF-TOKEN'))
+        ?.split('=')[1];
+}
 // Axiosインスタンスの作成
 const laravelAxios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-TOKEN': decodeURIComponent(getCsrfToken()),
     },
     withCredentials: true,
     withXSRFToken: true,
