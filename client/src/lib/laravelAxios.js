120f3deff9df383e51e0919603aa86b8c9b0606c
Shoei Akiwa <shoei@ShoeisMacBook-Air-3.local>
2024-03-31 16:24:07 +0900

    csrf

--- client/src/lib/laravelAxios.js
+++ client/src/lib/laravelAxios.js
@@ -6,30 +6,10 @@ const laravelAxios = Axios.create({
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
-    withCredentials: true, // クッキーを含むリクエストを送信するために必要
+    withCredentials: true, 
+    withXSRFToken: true
 });
 
-// CSRFトークンを取得してセットする関数
-export const fetchCsrfToken = async () => {
-    await laravelAxios.get('/sanctum/csrf-cookie');
-};
 
-// リクエストインターセプタを追加
-// すべてのリクエストの前にCSRFトークンがクッキーにセットされているかを確認し、
-// 必要に応じてCSRFトークンを取得します
-laravelAxios.interceptors.request.use(
-    async config => {
-        // CSRFトークンが必要なリクエストタイプの場合（通常は "GET" 以外）
-        // ここでは簡単化のためにすべてのリクエストで実行しています
-        await fetchCsrfToken();
-
-        // CSRFトークンがクッキーにセットされた後、リクエスト設定を返してリクエストを続行
-        return config;
-    },
-    error => {
-        // リクエストエラーの処理
-        return Promise.reject(error);
-    },
-);
 
 export default laravelAxios;
--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,15 +1,5 @@
-import React, { useEffect } from 'react';
-import 'tailwindcss/tailwind.css';
-// CSRFトークン取得関数のパスを適切に設定
-import { fetchCsrfToken } from '@/lib/laravelAxios';
+import 'tailwindcss/tailwind.css'
 
-const App = ({ Component, pageProps }) => {
-    useEffect(() => {
-        // コンポーネントがマウントされた後にCSRFトークンを取得
-        fetchCsrfToken().catch(console.error);
-    }, []); // 空の依存配列を渡して、効果がマウント時に1回だけ実行されるようにします。
+const App = ({ Component, pageProps }) => <Component {...pageProps} />
 
-    return <Component {...pageProps} />;
-};
-
-export default App;
+export default App
