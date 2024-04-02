bfdfeb31abf65ae75e654da98e4264305a85a9e0
Shoei Akiwa <shoei@ShoeisMacBook-Air-3.local>
2024-04-02 19:42:41 +0900

    csrf

--- client/src/lib/laravelAxios.js
+++ client/src/lib/laravelAxios.js
@@ -21,7 +21,7 @@ const laravelAxios = axios.create({
 
 // リクエストが送信される直前に実行されるインターセプターを追加
 laravelAxios.interceptors.request.use(config => {
-    const token = getCsrfTokenFromCookies();
+    const token = getCsrfToken();
     config.headers['X-Requested-With'] = 'XMLHttpRequest';
     config.headers['X-XSRF-TOKEN'] = token; // トークンを動的にヘッダーに添付
     return config;
