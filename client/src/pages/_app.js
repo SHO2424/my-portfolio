928aaf52ff59c66fd3d24a8306945b0e2b989718
Shoei Akiwa <shoei@ShoeisMacBook-Air-3.local>
2024-04-02 18:26:01 +0900

    csrf

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,21 +1,26 @@
-import 'tailwindcss/tailwind.css';
-import React, { useEffect } from 'react';
-import laravelAxios from '@/lib/laravelAxios';
-function App({ Component, pageProps }) {
-    useEffect(() => {
-        const getCsrfToken = async () => {
-            try {
-                await laravelAxios.get('/sanctum/csrf-cookie');
-                console.log('CSRFトークンを取得しました');
-            } catch (error) {
-                console.error('CSRFトークンの取得に失敗しました', error);
-            }
-        };
+// import 'tailwindcss/tailwind.css';
+// import React, { useEffect } from 'react';
+// import laravelAxios from '@/lib/laravelAxios';
+// function App({ Component, pageProps }) {
+//     useEffect(() => {
+//         const getCsrfToken = async () => {
+//             try {
+//                 await laravelAxios.get('/sanctum/csrf-cookie');
+//                 console.log('CSRFトークンを取得しました');
+//             } catch (error) {
+//                 console.error('CSRFトークンの取得に失敗しました', error);
+//             }
+//         };
 
-        getCsrfToken();
-    }, []);
+//         getCsrfToken();
+//     }, []);
 
-    return <Component {...pageProps} />;
-}
+//     return <Component {...pageProps} />;
+// }
 
-export default App;
+// export default App;
+import 'tailwindcss/tailwind.css'
+
+const App = ({ Component, pageProps }) => <Component {...pageProps} />
+
+export default App
