40a891b5b9c3740998bae68b1134f501761ef8b3
Shoei Akiwa <shoei@ShoeisMacBook-Air-3.local>
2024-04-03 00:27:15 +0900

    csrf

--- client/src/lib/laravelAxios.js
+++ client/src/lib/laravelAxios.js
@@ -31,7 +31,7 @@
 import Axios from 'axios';
 
 const laravelAxios = Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
