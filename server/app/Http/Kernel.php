f4457af1c9d55dfae7e5d9ace6c6ca99c9fd93e0
Shoei Akiwa <shoei@ShoeisMacBook-Air-3.local>
2024-04-07 17:07:49 +0900

    kernel

--- server/app/Http/Kernel.php
+++ server/app/Http/Kernel.php
@@ -39,8 +39,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Illuminate\Session\Middleware\StartSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
