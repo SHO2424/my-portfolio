5a33d7dadf95289012fdc491e4019e52efbc11b2
Shoei Akiwa <shoei@ShoeisMacBook-Air-3.local>
2024-04-07 17:04:24 +0900

    index

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,41 +1,40 @@
-import Head from 'next/head';
 import Link from 'next/link';
-import { useAuth } from '@/hooks/auth';
-import  AppLayout  from '@/components/Layouts/AppLayout';
-import { Grid, Box } from '@mui/material';
+// import { useAuth } from '@/hooks/auth';
+import AppLayout from '@/components/Layouts/AppLayout';
+import { Box } from '@mui/material';
 import Button from '@/components/Button';
 
 export default function Home() {
-    const { user } = useAuth({ middleware: 'guest' });
+    // const { user } = useAuth({ middleware: 'guest' });
 
     return (
         <>
             <AppLayout>
-                
-                        <Box sx={{height:"100vh"}}>  
-                        <Box sx={{display:"flex",justifyContent:"center" ,alignItems: 'center',height:"100%"}} >
-                            <Button>
+                <Box sx={{ height: '100vh' }}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            height: '100%',
+                        }}>
+                        <Button>
                             <Link
                                 href="/login"
                                 className="text-sm text-white-700">
                                 Login
                             </Link>
-
-                            </Button>
-                            <Button style={{marginLeft:"10px"}}>
+                        </Button>
+                        <Button style={{ marginLeft: '10px' }}>
                             <Link
                                 href="/register"
                                 className="text-sm text-white-700">
                                 Register
                             </Link>
-                            </Button>
-                            </Box>
-                        </Box>
-             
-            
+                        </Button>
+                    </Box>
+                </Box>
             </AppLayout>
-
-             
         </>
     );
 }
